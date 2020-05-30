90859a18f65dba2a64d791992ff29b2b82626202
Jesper Veggerby <jesper@veggerby.com>
2020-05-30 17:32:09 +0200

    added explicit auth0 remove cookie

--- src/Veggerby.Greenhouse.Web/ClientApp/src/components/Home.js
+++ src/Veggerby.Greenhouse.Web/ClientApp/src/components/Home.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useCookies } from 'react-cookie';
+
+const COOKIE_NAME = 'auth0.is.authenticated';
 
 export const Home = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(['COOKIE_NAME']);
+
     return (
         <div>
             <h1>Hello, world!</h1>
@@ -17,6 +23,7 @@ export const Home = () => {
                 <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
             </ul>
             <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+            <Button onClick={() => removeCookie(COOKIE_NAME)}>Remove Auth0 Cookie</Button>
         </div>
     );
 };
